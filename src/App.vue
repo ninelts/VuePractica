5e1b53c46937bb68df342fd19b1a026422051730
Claudio <ckladiioxage@gmail.com>
2020-01-08 17:18:00 -0300

    cli-vue empezando ejercios

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Cabecera></Cabecera>
+  </div>
+</template>
+
+<script>
+import Cabecera from "./components/Cabecera.vue";
+export default {
+  name: "app",
+  components: {
+    Cabecera
+  }
+};
+</script>
---
+++ src/components/Cabecera.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Frutas</h1>
+    </div>
+</template>
